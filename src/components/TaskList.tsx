f3b82b3dd3296a7d6581c7edfe0caaaaef84f7f7
Thiago Bueno Costa <th.bueno.95@gmail.com>
2021-07-01 00:00:59 -0300

    feat: added changed isComplete true and false

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -27,7 +27,11 @@ export function TaskList() {
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTask = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task );
+    setTasks(newTask);
   }
 
   function handleRemoveTask(id: number) {
